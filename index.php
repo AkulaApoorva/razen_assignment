523548ad8d8ccda1ec2f82a8b612b8d33667bb5c
vipanchi samindla <43497714+vipanchi-samindla@users.noreply.github.com>
2020-07-22 15:12:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col">
+	        		
+	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		                <ol class="carousel-indicators">
+		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+		                </ol>
+		                <div class="carousel-inner">
+		                  <div class="item active">
+		                    <img src="images/bg1.png" alt="First slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/bg2.png" alt="Second slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/bg3.png" alt="Third slide">
+		                  </div>
+		                </div>
+		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+		                  <span class="fa fa-angle-left"></span>
+		                </a>
+		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+		                  <span class="fa fa-angle-right"></span>
+		                </a>
+		            </div>
+		           
+		       		
+	        	</div>
+	        	
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
